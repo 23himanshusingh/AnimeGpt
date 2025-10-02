75f9ea66d4d9a9817f8a537042f0084e16818e00
HIMANSHU SINGH <100193490+23himanshusingh@users.noreply.github.com>
2025-10-02 17:21:22 +0530

    Update Vite configuration to set API proxy to localhost for local development, replacing the BACKEND_URL constant.

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { BACKEND_URL } from './src/utils/constants'
-
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server: {
     proxy: {
-      '/api': BACKEND_URL,
+      '/api': 'http://localhost:5000',
     },
   },
 })
